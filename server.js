8243f8f43fe29f936b3f2701f636756f6afa29b4
DongHun, Kim <donghun.kdh@gmail.com>
2022-06-12 00:25:44 +0900

    저장소 통합

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 const MongoClient = require('mongodb').MongoClient;
+let db;
 MongoClient.connect(process.env.DB_URL, (err, client) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  db = client.db('todolist');
+
   app.listen(8080, function () {
     console.log('listening on 8080');
   });
@@ -21,6 +28,13 @@ app.get('/write', (req, res) => {
 });
 
 app.post('/add', (req, res) => {
-  res.send('전송 완료');
-  console.log(req.body);
+  db.collection('post').insertOne(
+    { title: req.body.title, date: req.body.date },
+    (err, result) => {
+      if (err) {
+        return console.log(err);
+      }
+      res.send('저장 완료');
+    }
+  );
 });
