ba8cb82ba94efc228f8d4e81ef273465c0c3eb82
DongHun, Kim <donghun.kdh@gmail.com>
2022-06-12 17:27:19 +0900

    저장소 통합

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 const MongoClient = require('mongodb').MongoClient;
 let db;
