db057a807194f7fc213c0225f69fe6a6dd1c8934
DonhHun, Kim <donghun.kdh@gmail.com>
2022-08-02 04:52:50 +0900

    저장소 통합

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-const index = () => {
+const HomePage = () => {
   return (
     <div>
       <h1>The Home Page</h1>
@@ -6,4 +6,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default HomePage;
