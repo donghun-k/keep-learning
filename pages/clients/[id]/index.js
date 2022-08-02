a978bcdc39fbacbce90b1d760ca84558968122b3
DonhHun, Kim <donghun.kdh@gmail.com>
2022-08-03 03:18:06 +0900

    저장소 통합

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,13 @@ import { useRouter } from 'next/router';
 const ClientProjectsPage = () => {
   const router = useRouter();
 
+  function loadProjectHandler() {
+    router.push('/clients/donghun/projecta');
+  }
   return (
     <div>
       <h1>The Projects of a {router.query.id}</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
