1b7efbb24cf84f294129ba45e70b908656dc849d
DonhHun, Kim <donghun.kdh@gmail.com>
2022-08-04 04:54:54 +0900

    저장소 통합

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,32 @@
+import Link from 'next/link';
+
 const EventItem = (props) => {
-  return <li></li>;
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString('ko-KR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const formattedAddress = location.replace(', ', '\n');
+  const exploreLink = `/events/${id}`;
+  return (
+    <li>
+      <img src={'/' + image} alt='title' />
+      <div>
+        <h2>TITLE</h2>
+        <div>
+          <time>DATE</time>
+        </div>
+        <div>
+          <address>ADDRESS</address>
+        </div>
+      </div>
+      <div>
+        <Link href={exploreLink}>Explore Event</Link>
+      </div>
+    </li>
+  );
 };
 
 export default EventItem;
