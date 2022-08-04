27e4aaa53475e9b7f928c49d707f74f2c91e548f
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-05 04:33:01 +0900

    저장소 통합

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
-import { getAllEvents } from '../../dummy-data';
+import { useRouter } from 'next/router';
 import { Fragment } from 'react';
+import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/EventList';
 import EventsSearch from '../../components/events/EventsSearch';
 
 const AllEventPage = () => {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventHandler} />
       <EventList items={events} />
     </Fragment>
   );
