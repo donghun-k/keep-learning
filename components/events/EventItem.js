77a9efa08786d734ae2f0e1335e3efc586315412
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-05 01:00:21 +0900

    저장소 통합

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,4 +1,7 @@
-import Link from 'next/link';
+import Button from '../ui/button';
+import DateIcon from '../icons/DateIcon';
+import AddressIcon from '../icons/AddressIcon';
+import ArrowRightIcon from '../icons/ArrowRightIcon';
 import classes from './EventItem.module.css';
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -17,14 +20,21 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>TITLE</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
