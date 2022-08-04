656298ec634aa01f7ad248a63f80e2ec08f9e2e1
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-05 05:03:14 +0900

    저장소 통합

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,5 +1,11 @@
 import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+
 import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/EventList';
+import ResultsTitle from '../../components/events/ResultsTitle';
+import Button from '../../components/ui/Button';
+import ErrorAlert from '../../components/ui/ErrorAlert';
 
 const FilteredEventsPage = () => {
   const router = useRouter();
@@ -15,6 +21,7 @@ const FilteredEventsPage = () => {
   const numYear = +filteredYear;
   const numMonth = +FilteredMonth;
 
+  const date = new Date(numYear, numMonth - 1);
   if (
     isNaN(numYear) ||
     isNaN(numMonth) ||
@@ -22,7 +29,14 @@ const FilteredEventsPage = () => {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return <p>Invalid filter. Please adjust your values</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>Invalid filter. Please adjust your values</ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   const filteredEvents = getFilteredEvents({
@@ -31,12 +45,20 @@ const FilteredEventsPage = () => {
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found for th chose filter!</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>No events found for th chose filter!</ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 };
 
--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -4,6 +4,7 @@ import { getEventById } from '../../dummy-data';
 import EventSummary from '../../components/event-detail/EventSummary';
 import EventLogistics from '../../components/event-detail/EventLogistics';
 import EventContent from '../../components/event-detail/EventContent';
+import ErrorAlert from '../../components/ui/ErrorAlert';
 
 const EventDetailPAge = () => {
   const router = useRouter();
@@ -12,7 +13,7 @@ const EventDetailPAge = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found!</p>;
+    return <ErrorAlert>No event found!</ErrorAlert>;
   }
 
   return (
