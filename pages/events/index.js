8eed7c1e744f0768bac9909bb6995993c2c860cd
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-05 04:20:15 +0900

    저장소 통합

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,15 @@
 import { getAllEvents } from '../../dummy-data';
+import { Fragment } from 'react';
 import EventList from '../../components/events/EventList';
+import EventsSearch from '../../components/events/EventsSearch';
 
 const AllEventPage = () => {
   const events = getAllEvents();
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 };
 
