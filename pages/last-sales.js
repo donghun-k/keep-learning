a25bdbdd3c9719649961bc7e549c51dec95565a1
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-07 01:46:29 +0900

    저장소 통합

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,32 +1,52 @@
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 const LastSalesPage = () => {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    fetch('https://nextjs-course-b3c24-default-rtdb.firebaseio.com/sales.json')
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
+  const { data, error } = useSWR(
+    'https://nextjs-course-b3c24-default-rtdb.firebaseio.com/sales.json',
+    (url) => fetch(url).then((res) => res.json())
+  );
 
-  if (isLoading) {
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  // const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   fetch('https://nextjs-course-b3c24-default-rtdb.firebaseio.com/sales.json')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+  if (!data || !sales) {
     return <p>Loading...</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
-  }
   return (
     <ul>
       {sales.map((sale) => (
