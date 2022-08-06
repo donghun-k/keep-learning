0adb8613eb8ab4a68d01e8aba10d094ebf39c5d9
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-06 23:32:07 +0900

    저장소 통합

--- pages/[pid].js
+++ pages/[pid].js
@@ -17,14 +17,20 @@ const ProductDetailPage = (props) => {
     </Fragment>
   );
 };
+
+async function getData() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
 export async function getStaticProps(context) {
   const { params } = context;
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
@@ -36,9 +42,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [{ params: { pid: 'p1' } }],
-    fallback: 'blocking',
+    paths: pathWithParams,
+    fallback: false,
   };
 }
 export default ProductDetailPage;
