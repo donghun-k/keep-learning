10a0088ddce68518cebfe50e535ca41c67dc7e71
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-07 23:24:40 +0900

    저장소 통합

---
+++ helpers/api-util.js
@@ -0,0 +1,22 @@
+export async function getAllEvents() {
+  const res = await fetch(
+    'https://nextjs-course-b3c24-default-rtdb.firebaseio.com/events.json'
+  );
+  const data = await res.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
