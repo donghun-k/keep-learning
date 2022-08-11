02a2f803b4e09876588ad42e320d701494933ade
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-12 06:08:46 +0900

    저장소 통합

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -6,6 +8,18 @@ function handler(req, res) {
       res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://donghun:gFCZS27xZrseet1D@cluster0.cctfa7o.mongodb.net/?retryWrites=true&w=majority'
+    );
+
+    const db = client.db();
+
+    await db.collection('emails').insertOne({
+      email: userEmail,
+    });
+
+    client.close();
 
     console.log(userEmail);
     res.status(201).json({ message: 'Signed Up!' });
