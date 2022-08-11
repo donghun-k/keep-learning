7ce2327bfb8047b973a506a5d3b1b525a8c66cef
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-11 20:57:27 +0900

    저장소 통합

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,39 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === 'POST') {
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes('0') ||
+      !name ||
+      name.trim() === '' ||
+      !text ||
+      text.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
+
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+    console.log(newComment);
+
+    res.status(201).json({ message: 'Added comment.', comment: newComment });
+  }
+
+  if (req.method === 'GET') {
+    const dummyList = [
+      { id: 'c1', name: 'DongHun', text: 'A first comment!' },
+      { id: 'c2', name: 'JooHyeon', text: 'A Second comment!' },
+    ];
+
+    res.status.json({ comments: dummyList });
+  }
+}
+
+export default handler;
