d26a9c41d282502bb05ff72dd0665d89c9cbee7f
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-13 23:57:14 +0900

    저장소 통합

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,24 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 
 import MainHeader from './main-header';
+import Notification from '../../Components/ui/notification';
+import NotificationContext from '../../store/notification-context';
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activieNotification = notificationCtx.notification;
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activieNotification && (
+        <Notification
+          title={activieNotification.title}
+          message={activieNotification.message}
+          status={activieNotification.status}
+        />
+      )}
     </Fragment>
   );
 }
