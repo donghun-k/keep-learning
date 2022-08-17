f9d2166950cd61237078223d3f6c6ed163644989
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-17 19:50:57 +0900

    저장소 통합

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,30 @@
+import AllPosts from '../../components/posts/all-post';
+
+const DUMMY_POSTS = [
+  {
+    title: 'Getting started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Studying Next.js',
+    date: '2022-02-02',
+    slug: 'getting-started-with-nextjs',
+  },
+  {
+    title: 'Getting started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Studying Next.js',
+    date: '2022-02-02',
+    slug: 'getting-started-with-nextjs2',
+  },
+  {
+    title: 'Getting started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Studying Next.js',
+    date: '2022-02-02',
+    slug: 'getting-started-with-nextjs3',
+  },
+];
+
 const AllPostsPage = () => {
-  return <div></div>;
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 export default AllPostsPage;
