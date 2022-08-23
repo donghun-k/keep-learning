99a009b5584f0158f9d8d0c52b237d5ed79c0394
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-23 20:49:56 +0900

    저장소 통합

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,15 +1,13 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
 import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
-import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
 SyntaxHighlighter.registerLanguage('js', js);
-SyntaxHighlighter.registerLanguage('css', css);
 
 const PostContent = (props) => {
   const { post } = props;
@@ -46,12 +44,13 @@ const PostContent = (props) => {
       return <p>{paragraph.children}</p>;
     },
     code(code) {
-      const { language, value } = code;
+      const { className, children } = code;
+      const language = className.split('-')[1];
       return (
         <SyntaxHighlighter
           style={atomDark}
           language={language}
-          children={value}
+          children={children}
         />
       );
     },
