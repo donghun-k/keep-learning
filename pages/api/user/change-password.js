335633ccd8d62c6a3820de1169e62cd242e2d722
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-30 05:36:55 +0900

    저장소 통합

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,7 @@
 import { getSession } from 'next-auth/client';
+import { useSyncExternalStore } from 'react';
+import { hashPassword, verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
 
 async function handler(req, res) {
   if (req.method !== 'PATCH') {
@@ -11,6 +14,42 @@ async function handler(req, res) {
     res.status(401).json({ message: 'Not authenticated!' });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+
+  const userCollection = client.db('auth-demo').collection('users');
+
+  const user = await userCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found.' });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordsAreEqual) {
+    res.status(422).json({ message: 'Invalid password.' });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  const result = await userCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: 'Password updated!' });
 }
 
 export default handler;
