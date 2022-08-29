67d5e7bf88ecc9f667b48af7ad70f5a9c9cc1b71
DongHun, Kim <donghun.kdh@gmail.com>
2022-08-30 05:01:23 +0900

    저장소 통합

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,7 @@
 import { useState, useRef } from 'react';
 import { signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
+
 import classes from './auth-form.module.css';
 
 async function createUser(email, password) {
@@ -25,6 +27,7 @@ function AuthForm() {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -43,7 +46,9 @@ function AuthForm() {
         password: enteredPassword,
       });
 
-      console.log(result);
+      if (!result.error) {
+        router.replace('/profile');
+      }
     } else {
       try {
         const result = createUser(enteredEmail, enteredPassword);
