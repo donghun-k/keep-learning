b0ffbec81c9e4c04a2eceff6740f0166c496897f
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-09 22:13:39 +0900

    저장소 통합

---
+++ src/Test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Test = () => {
+  return (
+    <div>
+      <Typography
+        variant='h1'
+        sx={{ color: 'red', border: '1px solid black', borderRadius: 10 }}
+      >
+        Hello, world!
+      </Typography>
+    </div>
+  );
+};
+export default Test;
