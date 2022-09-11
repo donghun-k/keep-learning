3cf30c67fd553219e802737b901740793b8429aa
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-11 21:32:02 +0900

    저장소 통합

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,22 +1,55 @@
-import { TextField, Typography } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import React from 'react';
 import { useState } from 'react';
 
 const Test = () => {
-  const [name, setName] = useState('');
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+
   return (
     <div>
-      <TextField
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        type='text'
-        placeholder='Name'
-        variant='outlined'
-      />
-      <TextField type='email' placeholder='Email' variant='standard' />
-      <TextField type='password' placeholder='Password' variant='filled' />
-
-      <Typography>{name}</Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name='name'
+          value={inputs.name}
+          onChange={handleChange}
+          type='text'
+          placeholder='Name'
+          variant='outlined'
+        />
+        <TextField
+          name='email'
+          value={inputs.email}
+          onChange={handleChange}
+          type='email'
+          placeholder='Email'
+          variant='standard'
+        />
+        <TextField
+          name='password'
+          value={inputs.password}
+          onChange={handleChange}
+          type='password'
+          placeholder='Password'
+          variant='filled'
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
     </div>
   );
 };
