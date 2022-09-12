140446463ca43d56c67080cd61291bdbfd5abe6e
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-12 23:29:39 +0900

    저장소 통합

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,126 +1,22 @@
-import {
-  Button,
-  Checkbox,
-  FormControl,
-  FormControlLabel,
-  FormGroup,
-  FormLabel,
-  InputLabel,
-  MenuItem,
-  Radio,
-  RadioGroup,
-  Select,
-  TextField,
-} from '@mui/material';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import { useState } from 'react';
 
 const Test = () => {
-  const [inputs, setInputs] = useState({
-    name: '',
-    email: '',
-    password: '',
-    subscribe: false,
-    age: 0,
-    gender: '',
-  });
-
-  const handleChange = (e) => {
-    setInputs((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(inputs);
-  };
-
   return (
     <div>
-      <form
-        style={{ display: 'flex', flexDirection: 'column' }}
-        onSubmit={handleSubmit}
-      >
-        <TextField
-          name='name'
-          value={inputs.name}
-          onChange={handleChange}
-          type='text'
-          placeholder='Name'
-          variant='outlined'
-          sx={{ margin: 3 }}
-        />
-        <TextField
-          name='email'
-          value={inputs.email}
-          onChange={handleChange}
-          type='email'
-          placeholder='Email'
-          variant='standard'
-          sx={{ margin: 3 }}
-        />
-        <TextField
-          name='password'
-          value={inputs.password}
-          onChange={handleChange}
-          type='password'
-          placeholder='Password'
-          variant='filled'
-          sx={{ margin: 3 }}
-        />
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox
-                onChange={() =>
-                  setInputs((prev) => ({
-                    ...prev,
-                    subscribe: !inputs.subscribe,
-                  }))
-                }
-              />
-            }
-            label='Subscribe'
-          />
-        </FormGroup>
-
-        <FormControl fullWidth>
-          <InputLabel>Age</InputLabel>
-          <Select
-            name='age'
-            value={inputs.age}
-            label='Age'
-            onChange={handleChange}
+      <AppBar>
+        <Toolbar>
+          <Typography>LOGO</Typography>
+          <Button
+            variant='contained'
+            sx={{ marginLeft: 'auto' }}
+            color='warning'
           >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl>
-          <FormLabel>Gender</FormLabel>
-          <RadioGroup
-            onChange={handleChange}
-            defaultValue='female'
-            name='gender'
-          >
-            <FormControlLabel
-              value='female'
-              control={<Radio />}
-              label='Female'
-            />
-            <FormControlLabel value='male' control={<Radio />} label='Male' />
-            <FormControlLabel value='other' control={<Radio />} label='Other' />
-          </RadioGroup>
-        </FormControl>
-
-        <Button type='submit' variant='contained' sx={{ margin: 3 }}>
-          Submit
-        </Button>
-      </form>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
