04959175bd82e4cded3247d019b6373fcca371e0
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-12 23:43:31 +0900

    저장소 통합

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,20 +1,23 @@
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { AppBar, Tab, Tabs, Toolbar } from '@mui/material';
 import React from 'react';
 import { useState } from 'react';
 
 const Test = () => {
+  const [value, setValue] = useState(0);
   return (
     <div>
       <AppBar>
         <Toolbar>
-          <Typography>LOGO</Typography>
-          <Button
-            variant='contained'
-            sx={{ marginLeft: 'auto' }}
-            color='warning'
+          <Tabs
+            indicatorColor='inherit'
+            textColor='inherit'
+            value={value}
+            onChange={(e, val) => setValue(val)}
           >
-            Login
-          </Button>
+            <Tab label='First' />
+            <Tab label='Second' />
+            <Tab label='Third' />
+          </Tabs>
         </Toolbar>
       </AppBar>
     </div>
