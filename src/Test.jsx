cd7da4d90ff5001062960093987ded10e93117fa
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-17 03:48:21 +0900

    저장소 통합

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,19 +1,16 @@
-import { Box, Button, Modal, Typography } from '@mui/material';
-import { useState } from 'react';
+import { Link } from '@mui/material';
 
 const Test = () => {
-  const [open, setOpen] = useState(false);
   return (
     <div>
-      <Button onClick={() => setOpen(true)}>Open Modal</Button>
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <Box position='absolute' top='50%' left='50%'>
-          <Typography>It is a Modal!</Typography>
-          <Button variant='contained' onClick={() => setOpen(false)}>
-            Click Me
-          </Button>
-        </Box>
-      </Modal>
+      <Link
+        variant='h2'
+        color='secondary'
+        underline='none'
+        href='https://www.google.com'
+      >
+        Visit Google
+      </Link>
     </div>
   );
 };
