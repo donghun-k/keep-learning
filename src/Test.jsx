f711931a6b7ad98106f3d566c78a1c1f5ae27b5a
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-17 03:55:25 +0900

    저장소 통합

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,16 +1,23 @@
-import { Link } from '@mui/material';
+import { Container } from '@mui/system';
 
 const Test = () => {
   return (
     <div>
-      <Link
-        variant='h2'
-        color='secondary'
-        underline='none'
-        href='https://www.google.com'
-      >
-        Visit Google
-      </Link>
+      <Container sx={{ background: 'pink' }} maxWidth='xs'>
+        This is XS
+      </Container>
+      <Container sx={{ background: 'pink' }} maxWidth='sm'>
+        This is SM
+      </Container>
+      <Container sx={{ background: 'pink' }} maxWidth='md'>
+        This is MD
+      </Container>
+      <Container sx={{ background: 'pink' }} maxWidth='lg'>
+        This is LG
+      </Container>
+      <Container sx={{ background: 'pink' }} maxWidth='xl'>
+        This is XL
+      </Container>
     </div>
   );
 };
