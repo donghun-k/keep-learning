664a322d32020c692a76a309146d8c3bb9bd2e75
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-18 02:53:51 +0900

    저장소 통합

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,38 +1,40 @@
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Box,
-  Collapse,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
+  Typography,
 } from '@mui/material';
 import { useState } from 'react';
 
-const array = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
-
 const Test = () => {
-  const [open, setOpen] = useState(false);
+  const [accordion, setAccordion] = useState('');
+
   return (
     <div>
       <Box>
-        <ListItem divider>
-          <ListItemButton onClick={() => setOpen(true)}>
-            <ListItemIcon>{'>'}</ListItemIcon>
-            <ListItemText primary={'Expand List'} />
-          </ListItemButton>
-        </ListItem>
-        <Collapse in={open}>
-          <List sx={{ marginLeft: '2rem' }}>
-            {array.map((listElm) => (
-              <ListItem divider>
-                <ListItemButton onClick={() => setOpen(false)}>
-                  <ListItemText primary={listElm} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Collapse>
+        <Accordion
+          expanded={accordion === 'test1'}
+          onChange={() => setAccordion('test1')}
+        >
+          <AccordionSummary expandIcon={'>'}>
+            <Typography>Test 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>My name is Dong Hun, Kim</Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          expanded={accordion === 'test2'}
+          onChange={() => setAccordion('test2')}
+        >
+          <AccordionSummary expandIcon={'>'}>
+            <Typography>Test 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>My name is Dong Hun, Kim</Typography>
+          </AccordionDetails>
+        </Accordion>
       </Box>
     </div>
   );
