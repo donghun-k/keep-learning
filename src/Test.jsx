250f2f7b32c724aac25ef04759b20053ec6449ae
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-18 05:06:06 +0900

    저장소 통합

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,24 +1,31 @@
-import { Autocomplete, TextField } from '@mui/material';
+import {
+  Button,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemText,
+} from '@mui/material';
 import { Box } from '@mui/system';
+import { useState } from 'react';
 
-let movie = [
-  'Scarface',
-  'Once Upon A Time In America',
-  'Blade Runner',
-  'Godfather',
-];
+const arrOptions = ['Profile', 'Balance', 'Logout'];
 
 const Test = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
       <Box>
-        <Autocomplete
-          sx={{ width: 300 }}
-          options={movie}
-          renderInput={(params) => (
-            <TextField {...params} label='List of Movie' />
-          )}
-        />
+        <Button onClick={() => setOpen(true)}>Open</Button>
+        <Drawer anchor='right' open={open} onClose={() => setOpen(false)}>
+          <List>
+            {arrOptions.map((elm) => (
+              <ListItemButton onClickCapture={() => setOpen(false)}>
+                <ListItemText primary={elm} />
+              </ListItemButton>
+            ))}
+          </List>
+        </Drawer>
       </Box>
     </div>
   );
