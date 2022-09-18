4a61c598a82606f941abd18e50d80c24b980b23a
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-18 16:17:23 +0900

    저장소 통합

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,11 +1,19 @@
-import { Box, CircularProgress, LinearProgress } from '@mui/material';
+import { Box, Rating, Typography } from '@mui/material';
+import { useState } from 'react';
 
 const Test = () => {
+  const [value, setValue] = useState();
   return (
     <div>
       <Box>
-        <CircularProgress color='secondary' />
-        <LinearProgress />
+        <Rating
+          precision={0.5}
+          defaultChecked={2.5}
+          value={value}
+          size='large'
+          onChange={(e, val) => setValue(val)}
+        />
+        <Typography>Rated {value !== undefined ? value : 0} Stars</Typography>
       </Box>
     </div>
   );
