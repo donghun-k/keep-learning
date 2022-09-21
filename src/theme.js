6bea88ff5cd7ccc7be11d31d49d1a9bd988c7375
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-21 21:45:57 +0900

    저장소 통합

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f4005e',
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '5rem',
+    },
+  },
+});
