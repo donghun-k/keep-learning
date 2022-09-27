7e0ccf56414428e740b250e7c8009b4b6dc8cd07
DongHun, Kim <donghun.kdh@gmail.com>
2022-09-28 05:36:26 +0900

    저장소 통합

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,14 +4,28 @@ import { Box } from '@mui/system';
 const Footer = () => {
   return (
     <Box display='flex' flexDirection='column'>
-      <Typography variant='h3' paddingTop={10} textAlign='center'>
+      <Typography
+        fontWeight='bold'
+        fontSize={{ lg: 30, md: 24, sm: 18, xs: 14 }}
+        variant='h3'
+        paddingTop={10}
+        textAlign='center'
+      >
         Ready For Any Team Size
       </Typography>
-      <Typography variant='div' textAlign='center' padding={4}>
+      <Typography
+        fontSize={{ lg: 24, md: 20, sm: 16 }}
+        variant='div'
+        textAlign='center'
+        padding={4}
+      >
         Optimized For Any Team Size
       </Typography>
       <Box display='flex' margin='auto' justifyContent='center' padding={5}>
-        <Button sx={{ mr: 2 }} variant='contained'>
+        <Button
+          sx={{ mr: 2, fontSize: { lg: 20, md: 17, sm: 12, xs: 8 } }}
+          variant='contained'
+        >
           Try CodeEnv For Free
         </Button>
         <Button sx={{ ml: 2 }} variant='outlined'>
