ab9440c6f2bf35f89f5d78a437a3db61779843b1
DongHun, Kim <donghun.kdh@gmail.com>
2022-10-23 21:16:31 +0900

    저장소 통합

--- mini-project/dist/index.js
+++ mini-project/dist/index.js
@@ -3,13 +3,21 @@ const btn = document.getElementById('btn');
 const input = document.getElementById('todoinput');
 const form = document.querySelector('form');
 const list = document.getElementById('todolist');
+const todos = [];
 const handleSubmit = (e) => {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+};
+const createTodo = (todo) => {
     const newLi = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    newLi.append(newTodoText);
+    newLi.append(todo.text);
     list === null || list === void 0 ? void 0 : list.append(newLi);
     newLi.append(checkbox);
 };
--- mini-project/src/index.ts
+++ mini-project/src/index.ts
@@ -1,15 +1,30 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
 const btn = document.getElementById('btn')!;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
 const form = document.querySelector('form')!;
 const list = document.getElementById('todolist')!;
 
+const todos: Todo[] = [];
+
 const handleSubmit = (e: SubmitEvent) => {
   e.preventDefault();
-  const newTodoText = input.value;
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+};
+
+const createTodo = (todo: Todo) => {
   const newLi = document.createElement('li');
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
-  newLi.append(newTodoText);
+  newLi.append(todo.text);
   list?.append(newLi);
   newLi.append(checkbox);
 };
