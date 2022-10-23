f3416ae7e7c27a35837131c03114b2fe8ad74bc5
DongHun, Kim <donghun.kdh@gmail.com>
2022-10-23 19:39:54 +0900

    저장소 통합

--- mini-project/dist/index.js
+++ mini-project/dist/index.js
@@ -2,8 +2,15 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoinput');
 const form = document.querySelector('form');
+const list = document.getElementById('todolist');
 const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Submitted!');
+    const newTodoText = input.value;
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    newLi.append(newTodoText);
+    list === null || list === void 0 ? void 0 : list.append(newLi);
+    newLi.append(checkbox);
 };
 form.addEventListener('submit', handleSubmit);
--- mini-project/src/index.ts
+++ mini-project/src/index.ts
@@ -1,10 +1,17 @@
 const btn = document.getElementById('btn')!;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
 const form = document.querySelector('form')!;
+const list = document.getElementById('todolist')!;
 
 const handleSubmit = (e: SubmitEvent) => {
   e.preventDefault();
-  console.log('Submitted!');
+  const newTodoText = input.value;
+  const newLi = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  newLi.append(newTodoText);
+  list?.append(newLi);
+  newLi.append(checkbox);
 };
 
 form.addEventListener('submit', handleSubmit);
