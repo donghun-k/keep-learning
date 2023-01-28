7c5402115032027982bdb77b1a115516f05dbd87
DongHun, Kim <donghun.kdh@gmail.com>
2023-01-28 22:15:57 +0900

    저장소 통합

--- Next.js/NextAuth practice/lib/db.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-export async function connectToDatabase() {
-  const client = await MongoClient.connect(
-    `mongodb+srv://donghun:${process.env.DB_URI}@cluster0.ls4pgza.mongodb.net/?retryWrites=true&w=majority`
-  );
-
-  return client;
-}
