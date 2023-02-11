ab06a2b9622d0ed54ee6e69180a6ac1f7aa9d253
DongHun, Kim <donghun.kdh@gmail.com>
2023-02-11 23:37:08 +0900

    저장소 통합

--- Next.js/blahx2/pages/[screenName]/index.tsx
+++ Next.js/blahx2/pages/[screenName]/index.tsx
@@ -1,11 +1,24 @@
 import { GetServerSideProps, NextPage } from 'next';
-import { Avatar, Box, Text, Flex, Textarea, Button, useToast, FormControl, Switch, FormLabel } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Text,
+  Flex,
+  Textarea,
+  Button,
+  useToast,
+  FormControl,
+  Switch,
+  FormLabel,
+  VStack,
+} from '@chakra-ui/react';
 import ResizeTextArea from 'react-textarea-autosize';
 import { useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import ServiceLayout from '@/components/service_layout';
 import { UseAuth } from '@/contexts/auth_user.context';
 import { InAuthUser } from '@/models/in_auth_user';
+import MessageItem from '@/components/message_item';
 
 interface Props {
   userInfo: InAuthUser | null;
@@ -164,6 +177,10 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
             </FormLabel>
           </FormControl>
         </Box>
+        <VStack spacing="12px" mt="6">
+          <MessageItem />
+          <MessageItem />
+        </VStack>
       </Box>
     </ServiceLayout>
   );
