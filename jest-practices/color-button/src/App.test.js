0501c5ab3443ad1f375e8f14f3275a37b7d23a78
DongHun, Kim <donghun.kdh@gmail.com>
2023-03-06 13:45:13 +0900

    저장소 통합

--- jest-practices/color-button/src/App.test.js
+++ jest-practices/color-button/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import { logRoles } from '@testing-library/react';
 import App from './App';
 
 test('Button has correct initial color', () => {
-  render(<App />);
+  const { container } = render(<App />);
+  logRoles(container);
   // button 이라는 Role과 'Change to blue'라는 텍스트로 버튼 찾기
   const colorBtn = screen.getByRole('button', { name: 'Change to blue' });
   // 버튼 색이 빨강인지 확인
