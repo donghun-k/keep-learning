03e0cb09d7143806fcfcc1a62cf27c68303b3a15
DongHun, Kim <donghun.kdh@gmail.com>
2023-03-06 17:15:02 +0900

    저장소 통합

--- jest-practices/color-button/src/App.test.js
+++ jest-practices/color-button/src/App.test.js
@@ -5,16 +5,28 @@ test('Button has correct initial color', () => {
   render(<App />);
   // button 이라는 Role과 'Change to blue'라는 텍스트로 버튼 찾기
   const colorBtn = screen.getByRole('button', { name: 'Change to blue' });
-  // 버튼 색이 빨강인지 확인
+  // 버튼 색이 빨강인지 테스트
   expect(colorBtn).toHaveStyle({ backgroundColor: 'red' });
 });
+
 test('Button turns blue when clicked', () => {
   render(<App />);
   const colorBtn = screen.getByRole('button', { name: 'Change to blue' });
   // 버튼 클릭
   fireEvent.click(colorBtn);
-  // 버튼 색이 파랑으로 변했는지 확인
+  // 버튼 색이 파랑으로 변했는지 테스트
   expect(colorBtn).toHaveStyle({ backgroundColor: 'blue' });
-  // 텍스트도 변경되었는지 확인
+  // 텍스트도 변경되었는지 테스트
   expect(colorBtn).toHaveTextContent('Change to red');
+});
+
+test('Initial conditions', () => {
+  render(<App />);
+  // 버튼이 활성화된 상태인지 테스트
+  const colorBtn = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorBtn).toBeEnabled();
+
+  // 체크박스가 unchecked 상태인지 테스트
+  const checkBox = screen.getByRole('checkBox');
+  expect(checkBox).not.toBeChecked();
 });
