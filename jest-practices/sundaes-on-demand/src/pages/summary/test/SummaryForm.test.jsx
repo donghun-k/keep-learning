5ddd4f1beba00227df1e1715397e55deb4d4ae63
DongHun, Kim <donghun.kdh@gmail.com>
2023-03-09 19:53:03 +0900

    저장소 통합

--- jest-practices/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ jest-practices/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,4 @@
-import { render, test, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import SummaryForm from '../SummaryForm';
 
 test('초기 조건 확인', () => {
@@ -7,4 +7,20 @@ test('초기 조건 확인', () => {
     name: /terms and conditions/i,
   });
   expect(checkbox).not.toBeChecked();
+  const confirmBtn = screen.getByRole('button');
+  expect(confirmBtn).toBeDisabled();
+});
+
+test('체크박스 체크 여부에 따른 버튼 활성화', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  const confirmBtn = screen.getByRole('button', {
+    name: /confirm order/i,
+  });
+  fireEvent.click(checkbox);
+  expect(confirmBtn).toBeEnabled();
+  fireEvent.click(checkbox);
+  expect(confirmBtn).toBeDisabled();
 });
