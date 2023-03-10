a61a8406eac858fc708d6b59e44525d87aea681f
DongHun, Kim <donghun.kdh@gmail.com>
2023-03-10 14:40:38 +0900

    저장소 통합

--- jest-practices/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ jest-practices/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -26,3 +26,21 @@ test('체크박스 체크 여부에 따른 버튼 활성화', async () => {
   await user.click(checkbox);
   expect(confirmBtn).toBeDisabled();
 });
+
+test('마우스 호버 시 팝오버', async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered./
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});
