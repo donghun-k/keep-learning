6599235daac05dd33aeb25b482f87cb81e821baa
DongHun, Kim <donghun.kdh@gmail.com>
2023-03-15 14:26:33 +0900

    저장소 통합

--- jest-practices/sundaes-on-demand/src/pages/entry/tests/Option.test.jsx
+++ jest-practices/sundaes-on-demand/src/pages/entry/tests/Option.test.jsx
@@ -2,10 +2,10 @@ import { render, screen } from '@testing-library/react';
 
 import Options from '../Options';
 
-test('각 스쿱 옵션의 이미지 표시', () => {
+test('각 스쿱 옵션의 이미지 표시', async () => {
   render(<Options optionType="scoops" />);
 
-  const scoopImages = screen.getAllByRole('img', { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   const altText = scoopImages.map((scoopImage) => scoopImage.alt);
