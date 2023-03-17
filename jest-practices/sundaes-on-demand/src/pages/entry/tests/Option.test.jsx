9a69c3a104d2d0e62d4cb9c4ff136dc39ada422b
DongHun, Kim <donghun.kdh@gmail.com>
2023-03-17 16:23:52 +0900

    저장소 통합

--- jest-practices/sundaes-on-demand/src/pages/entry/tests/Option.test.jsx
+++ jest-practices/sundaes-on-demand/src/pages/entry/tests/Option.test.jsx
@@ -11,3 +11,17 @@ test('각 스쿱 옵션의 이미지 표시', async () => {
   const altText = scoopImages.map((scoopImage) => scoopImage.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('각 토핑 옵션의 이미지 표시', async () => {
+  render(<Options optionType="toppings" />);
+
+  const images = await screen.findAllByRole('img', { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  const imageTitles = images.map((image) => image.alt);
+  expect(imageTitles).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
