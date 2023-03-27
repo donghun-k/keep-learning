239e6fe73f7a6cebdd2a72ba4431197017e4db81
DongHun, Kim <donghun.kdh@gmail.com>
2023-03-27 22:23:19 +0900

    저장소 통합

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -17,6 +17,19 @@ searchInputEl.addEventListener('blur', () => {
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', () => {
-  console.log('scroll!');
-});
+window.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
