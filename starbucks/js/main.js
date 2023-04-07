978c6d7230ea40d74f7278fa443c2a13e28e6224
DongHun, Kim <donghun.kdh@gmail.com>
2023-04-08 01:59:10 +0900

    저장소 통합

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -118,3 +118,6 @@ spyEls.forEach((spyEl) => {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
