8b73faebaa9880270f527eb432a00584626a2d62
DongHun, Kim <donghun.kdh@gmail.com>
2023-04-12 17:28:59 +0900

    저장소 통합

--- apple-ipad/js/main.js
+++ apple-ipad/js/main.js
@@ -1,6 +1,47 @@
-const basketStarter = document.querySelector('.basket-starter');
-const basket = basketStarter.querySelector('.basket');
+// Basket
+const basketStarterEl = document.querySelector('.basket-starter');
+const basketEl = basketStarterEl.querySelector('.basket');
 
-basketStarter.addEventListener('click', () => {
-  basket.classList.toggle('show');
+basketStarterEl.addEventListener('click', (e) => {
+  e.stopPropagation();
+  if (basketEl.classList.contains('show')) {
+    hideBasket();
+  } else {
+    showBasket();
+  }
 });
+
+basketEl.addEventListener('click', (e) => {
+  e.stopPropagation();
+});
+
+window.addEventListener('click', () => {
+  hideBasket();
+});
+
+function showBasket() {
+  basketEl.classList.add('show');
+}
+function hideBasket() {
+  basketEl.classList.remove('show');
+}
+
+// Search
+const headerEl = document.querySelector('header');
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+  document.documentElement.classList.add('fixed');
+}
+function hideSearch() {
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
+}
