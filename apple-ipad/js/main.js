b296835b6cec9dc185c9bc1dd41a56d4ec023f49
DongHun, Kim <donghun.kdh@gmail.com>
2023-04-13 15:55:55 +0900

    저장소 통합

--- apple-ipad/js/main.js
+++ apple-ipad/js/main.js
@@ -28,10 +28,12 @@ function hideBasket() {
 
 // Search
 const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')];
 const searchWrapEl = headerEl.querySelector('.search-wrap');
 const searchStarterEl = headerEl.querySelector('.search-starter');
 const searchCloserEl = searchWrapEl.querySelector('.search-closer');
 const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
 
 searchStarterEl.addEventListener('click', showSearch);
 searchCloserEl.addEventListener('click', hideSearch);
@@ -40,8 +42,21 @@ searchShadowEl.addEventListener('click', hideSearch);
 function showSearch() {
   headerEl.classList.add('searching');
   document.documentElement.classList.add('fixed');
+  headerMenuEls.reverse().forEach((el, i) => {
+    el.style.transitionDelay = (i * 0.4) / headerMenuEls.length + 's';
+  });
+  searchDelayEls.forEach((el, i) => {
+    el.style.transitionDelay = (i * 0.4) / searchDelayEls.length + 's';
+  });
 }
 function hideSearch() {
   headerEl.classList.remove('searching');
   document.documentElement.classList.remove('fixed');
+  headerMenuEls.reverse().forEach((el, i) => {
+    el.style.transitionDelay = (i * 0.4) / headerMenuEls.length + 's';
+  });
+  searchDelayEls.reverse().forEach((el, i) => {
+    el.style.transitionDelay = (i * 0.4) / searchDelayEls.length + 's';
+  });
+  searchDelayEls.reverse();
 }
