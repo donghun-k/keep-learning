dce711982dafcc679188b6ddb7617645852ded53
DongHun, Kim <donghun.kdh@gmail.com>
2023-05-03 13:44:01 +0900

    저장소 통합

--- js-ts/movie-app/api/movie.js
+++ js-ts/movie-app/api/movie.js
@@ -2,12 +2,12 @@ import fetch from 'node-fetch';
 
 const { APIKEY } = process.env;
 
-export default async function handler(req, res) {
-  const { title, page, id } = JSON.parse(req.body);
+export default async function handler(request, response) {
+  const { title, page, id } = JSON.parse(request.body);
   const url = id
     ? `https://omdbapi.com?${APIKEY}=7035c60c&i=${id}&plot=full`
     : `https://omdbapi.com?${APIKEY}=7035c60c&s=${title}&page=${page}`;
   const res = await fetch(url);
   const data = await res.json();
-  res.status(200).json(data);
+  response.status(200).json(data);
 }
