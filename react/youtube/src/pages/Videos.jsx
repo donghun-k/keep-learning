8e3455963c80d5c73f11ed48e0202957ce04011b
DongHun, Kim <donghun.kdh@gmail.com>
2023-05-13 19:37:01 +0900

    저장소 통합

--- react/youtube/src/pages/Videos.jsx
+++ react/youtube/src/pages/Videos.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Video = () => {
-  return <div>Video</div>;
+  const { keyword } = useParams();
+  return <div>Video {keyword}</div>;
 };
 
 export default Video;
