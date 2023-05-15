ac75df8a6581be8d36b3bd34f5234866115bcf9f
DongHun, Kim <donghun.kdh@gmail.com>
2023-05-15 19:39:22 +0900

    저장소 통합

--- react/youtube/README.md
+++ react/youtube/README.md
@@ -10,6 +10,9 @@ __⟪드림코딩 아카데미 - 리액트 개념정리 · 클론코딩⟫__ 실
   - `useState`, `useEffect` 훅을 사용한 내부 상태 관리
   - **Context API**를 사용한 전역 상태 관리
 - **React Router**를 사용한 라우팅
+  - `useNavigate` 훅, `Link` 컴포넌트를 사용한 경로 이동
 - **React Query**를 사용한 서버 상태 관리
 - **Axios**를 사용한 HTTP 비동기 통신
-  -**Mock data**를 사용한 Fake 서비스
+  - **Axios 인스턴스** 사용
+  - **Mock data**를 사용한 Fake 서비스
+- **Tailwind CSS**로 빠르고 간편한 스타일링!
--- react/youtube/src/pages/VideoDetail.jsx
+++ react/youtube/src/pages/VideoDetail.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
 
 const VideoDetail = () => {
-  return <div>VideoDetail</div>;
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
+  return (
+    <section>
+      <article>
+        <iframe
+          title={title}
+          id="player"
+          type="text/html"
+          width="100%"
+          height="640"
+          src={`http://www.youtube.com/embed/${video.id}`}
+        ></iframe>
+        <div>
+          <h2>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle} />
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
 };
 
 export default VideoDetail;
