7c72de6e44be339e3298cd7cb6ce783b716e89e8
DongHun, Kim <donghun.kdh@gmail.com>
2023-05-17 22:14:01 +0900

    저장소 통합

--- react/shoppy/src/components/Navbar.jsx
+++ react/shoppy/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiShoppingBag } from 'react-icons/fi';
+import { BsFillPencilFill } from 'react-icons/bs';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <Link to="/">
+        <FiShoppingBag />
+        <h1>Shoppy</h1>
+      </Link>
+      <nav>
+        <Link to="/products">Products</Link>
+        <Link to="/carts">Carts</Link>
+        <Link to="/products/new">
+          <BsFillPencilFill />
+        </Link>
+        <button>Login</button>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
