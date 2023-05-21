0767b86d0602ca59b460a6f6d66c4a73e80bc39f
DongHun, Kim <donghun.kdh@gmail.com>
2023-05-21 21:14:55 +0900

    저장소 통합

--- react/shoppy/src/pages/MyCart.jsx
+++ react/shoppy/src/pages/MyCart.jsx
@@ -6,6 +6,7 @@ import CartItem from '../components/CartItem';
 import PriceCard from '../components/PriceCard';
 import { BsFillPlusCircleFill } from 'react-icons/bs';
 import { FaEquals } from 'react-icons/fa';
+import Button from '../components/ui/Button';
 
 const SHIPPING = 3000;
 const MyCart = () => {
@@ -19,23 +20,26 @@ const MyCart = () => {
     products &&
     products.reduce((acc, cur) => acc + parseInt(cur.price) * cur.quantity, 0);
   return (
-    <section>
-      <p>내 장바구니</p>
+    <section className="p-8 flex flex-col">
+      <p className="text-2xl text-center font-bold pb-4 border-b border-gray-300">
+        내 장바구니
+      </p>
       {!hasProducts && <p>장바구니가 비어있습니다.</p>}
       {hasProducts && (
         <>
-          <ul>
+          <ul className="border-b border-gray-300 mb-8 p-4 px-8">
             {products.map((product) => (
               <CartItem key={product.id} product={product} uid={uid} />
             ))}
           </ul>
-          <div>
+          <div className="flex justify-between items-center mb-6 px-2 md:p-8 lg:px-16">
             <PriceCard text="상품 총액" price={totalPrice} />
-            <BsFillPlusCircleFill />
+            <BsFillPlusCircleFill className="shrink-0" />
             <PriceCard text="배송비" price={SHIPPING} />
-            <FaEquals />
+            <FaEquals className="shrink-0" />
             <PriceCard text="결제 예정 금액" price={totalPrice + SHIPPING} />
           </div>
+          <Button text="주문하기" />
         </>
       )}
     </section>
