82b7e434022ccafbbc21b586201fd0df60c3eeca
DongHun, Kim <donghun.kdh@gmail.com>
2023-05-21 20:54:49 +0900

    저장소 통합

---
+++ react/shoppy/src/components/CartItem.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import { addOrUpdateToCart, removeItemFromCart } from '../api/firebase';
+
+const CartItem = ({
+  product,
+  product: { id, image, title, option, quantity, price },
+  uid,
+}) => {
+  const handleMinus = () => {
+    if (quantity < 2) return;
+    addOrUpdateToCart(uid, { ...product, quantity: quantity - 1 });
+  };
+  const handlePlus = () => {
+    addOrUpdateToCart(uid, { ...product, quantity: quantity + 1 });
+  };
+  const handleDelete = () => {
+    removeItemFromCart(uid, id);
+  };
+
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{option}</p>
+        <div>
+          <AiOutlineMinusSquare onClick={handleMinus} />
+          <span>{quantity}</span>
+          <AiOutlinePlusSquare onClick={handlePlus} />
+          <RiDeleteBin5Fill onClick={handleDelete} />
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
--- react/shoppy/src/pages/MyCart.jsx
+++ react/shoppy/src/pages/MyCart.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { getCart } from '../api/firebase';
+import { useQuery } from '@tanstack/react-query';
+import { useAuthContext } from '../context/AuthContext';
+import CartItem from '../components/CartItem';
+import PriceCard from '../components/PriceCard';
+import { BsFillPlusCircleFill } from 'react-icons/bs';
+import { FaEquals } from 'react-icons/fa';
 
+const SHIPPING = 3000;
 const MyCart = () => {
-  return <div>MyCart</div>;
+  const { uid } = useAuthContext();
+  const { isLoading, data: products } = useQuery(['carts'], () => getCart(uid));
+
+  if (isLoading) return <p>Loading...</p>;
+
+  const hasProducts = products && products.length > 0;
+  const totalPrice =
+    products &&
+    products.reduce((acc, cur) => acc + parseInt(cur.price) * cur.quantity, 0);
+  return (
+    <section>
+      <p>내 장바구니</p>
+      {!hasProducts && <p>장바구니가 비어있습니다.</p>}
+      {hasProducts && (
+        <>
+          <ul>
+            {products.map((product) => (
+              <CartItem key={product.id} product={product} uid={uid} />
+            ))}
+          </ul>
+          <div>
+            <PriceCard text="상품 총액" price={totalPrice} />
+            <BsFillPlusCircleFill />
+            <PriceCard text="배송비" price={SHIPPING} />
+            <FaEquals />
+            <PriceCard text="결제 예정 금액" price={totalPrice + SHIPPING} />
+          </div>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default MyCart;
