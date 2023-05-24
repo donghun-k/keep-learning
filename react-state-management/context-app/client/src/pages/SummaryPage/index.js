49f3337c5c623b1cd33f2e27b0160be4d935358d
DongHun, Kim <donghun.kdh@gmail.com>
2023-05-24 22:47:08 +0900

    저장소 통합

--- react-state-management/context-app/client/src/pages/SummaryPage/index.js
+++ react-state-management/context-app/client/src/pages/SummaryPage/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 
 const SummaryPage = () => {
-  return <div>SummaryPage</div>;
+  const [checked, setChecked] = useState(false);
+  return (
+    <div>
+      <form>
+        <input
+          type="checkbox"
+          checked={checked}
+          id="confirm-checkbox"
+          onClick={(e) => setChecked(e.target.checked)}
+        />
+        <label htmlFor="confirm-checkbox">
+          주문 내역을 확인하였으며, 결제에 동의합니다.
+        </label>
+        <br />
+        <button disabled={!checked} type="submit">
+          결제하기
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SummaryPage;
