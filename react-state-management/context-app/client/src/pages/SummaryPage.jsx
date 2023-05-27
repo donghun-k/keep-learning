10f4dec2698680367c89965087986285b63014d9
DongHun, Kim <donghun.kdh@gmail.com>
2023-05-28 00:54:50 +0900

    저장소 통합

--- react-state-management/context-app/client/src/pages/SummaryPage.jsx
+++ react-state-management/context-app/client/src/pages/SummaryPage.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useState } from 'react';
+import { OrderContext } from '../contexts/OrderContext';
 
-const SummaryPage = () => {
+const SummaryPage = ({ setStep }) => {
   const [checked, setChecked] = useState(false);
+  const [orderDetails] = useContext(OrderContext);
+
+  const productArray = Array.from(orderDetails.products);
+  const productList = productArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const hasOptions = orderDetails.options.size > 0;
+  let optonsDisplay = null;
+
+  if (hasOptions) {
+    const optionArray = Array.from(orderDetails.options.keys());
+    const optionsList = optionArray.map((option) => (
+      <li key={option}>{option}</li>
+    ));
+    optonsDisplay = (
+      <>
+        <h2>옵션: {orderDetails.totals.options}</h2>
+        <ul>{optionsList}</ul>
+      </>
+    );
+  }
+
   return (
     <div>
-      <form>
+      <h1>주문 내역</h1>
+      <h2>여행 상품: {orderDetails.totals.products} </h2>
+      <ul>{productList}</ul>
+      {optonsDisplay}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setStep(2);
+        }}
+      >
         <input
           type="checkbox"
           checked={checked}
