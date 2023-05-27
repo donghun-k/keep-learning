e9179ffbdd83fc8d8141230b588353f5be69a7eb
DongHun, Kim <donghun.kdh@gmail.com>
2023-05-28 00:35:03 +0900

    저장소 통합

--- react-state-management/context-app/client/src/components/Type.jsx
+++ react-state-management/context-app/client/src/components/Type.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import Products from './Products';
 import Options from './Options';
 import ErrorBanner from './ErrorBanner';
+import { OrderContext } from '../contexts/OrderContext';
 
 const Type = ({ orderType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderData, updateItemCount] = useContext(OrderContext);
 
   const loadItems = async () => {
     try {
@@ -28,6 +30,9 @@ const Type = ({ orderType }) => {
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, orderType)
+      }
     />
   ));
 
@@ -37,9 +42,9 @@ const Type = ({ orderType }) => {
 
   return (
     <div>
-      <h2>주문 종류</h2>
+      <h2>주문 종류:</h2>
       <p>하나의 가격</p>
-      <p>총 가격:</p>
+      <p>총 가격: {orderData.totals[orderType]}</p>
       <div
         style={{
           display: 'flex',
