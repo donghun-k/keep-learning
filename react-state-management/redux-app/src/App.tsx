0f54fac3b131433e6c3e88a8c7f4a62b191b7a94
DongHun, Kim <donghun.kdh@gmail.com>
2023-06-01 21:12:51 +0900

    저장소 통합

--- react-state-management/redux-app/src/App.tsx
+++ react-state-management/redux-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 type Props = {
@@ -8,11 +8,23 @@ type Props = {
 };
 
 function App({ value, onIncrement, onDecrement }: Props) {
+  const [todoValue, setTodoValue] = useState('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodoValue('');
+  };
   return (
     <div className="App">
       <h1>Clicked: {value} times</h1>
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <button type="submit" />
+      </form>
     </div>
   );
 }
--- react-state-management/redux-app/src/index.tsx
+++ react-state-management/redux-app/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,11 +15,13 @@ const store = createStore(rootReducer);
 const render = () =>
   root.render(
     <React.StrictMode>
-      <App
-        value={store.getState()}
-        onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-        onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-      />
+      <Provider store={store}>
+        <App
+          value={store.getState()}
+          onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+          onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+        />
+      </Provider>
     </React.StrictMode>
   );
 
