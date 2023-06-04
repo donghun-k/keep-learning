eda8452dc2b981730c4ce2261b87ceaf113dc480
DongHun, Kim <donghun.kdh@gmail.com>
2023-06-04 22:22:37 +0900

    저장소 통합

--- react-state-management/redux-app/src/actions/posts.tsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const fetchPosts = (): any => async (dispatch: any, getState: any) => {
-  const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-  dispatch({ type: 'FETCH_POSTS', payload: res.data });
-};
