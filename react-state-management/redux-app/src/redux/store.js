f6854cef1aeaf303d06029bab89745219de9dbb9
DongHun, Kim <donghun.kdh@gmail.com>
2023-06-13 16:58:02 +0900

    저장소 통합

--- react-state-management/redux-app/src/redux/store.js
+++ react-state-management/redux-app/src/redux/store.js
@@ -2,3 +2,5 @@ import { createStore } from 'redux';
 import userReducer from './user/userReducer';
 
 export const store = createStore(userReducer);
+
+
---
+++ react-state-management/redux-app/src/redux/userActions.js
@@ -0,0 +1,17 @@
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+// 로그인 액션 생성 함수
+export function loginAction(userData) {
+  return {
+    type: LOGIN,
+    payload: userData,
+  };
+}
+
+// 로그아웃 액션 생성 함수
+export function logoutAction() {
+  return {
+    type: LOGOUT,
+  };
+}
