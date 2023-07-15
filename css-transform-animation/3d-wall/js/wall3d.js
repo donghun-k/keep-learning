a45c7cfa1233eb3274b0cfbc6bbc797181dd46a5
DongHun, Kim <donghun.kdh@gmail.com>
2023-07-15 14:36:00 +0900

    저장소 통합

--- css-transform-animation/3d-wall/js/wall3d.js
+++ css-transform-animation/3d-wall/js/wall3d.js
@@ -1,6 +1,8 @@
 (function () {
   const houseEl = document.querySelector('.house');
+  const stageEl = document.querySelector('.stage');
   const barEl = document.querySelector('.progress-bar');
+  const mousePos = { x: 0, y: 0 };
   let maxScrollValue;
 
   function resizeHandler() {
@@ -9,10 +11,18 @@
 
   window.addEventListener('scroll', () => {
     const scrollPercent = scrollY / maxScrollValue;
-    const zMove = scrollPercent * 980 - 500;
+    const zMove = scrollPercent * 930 - 500 + 50;
     houseEl.style.transform = `translateZ(${zMove}vw)`;
 
     barEl.style.width = scrollPercent * 100 + '%';
+  });
+
+  window.addEventListener('mousemove', (e) => {
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageEl.style.transform = `rotateX(${mousePos.y * 5}deg) rotateY(${
+      mousePos.x * 5
+    }deg)`;
   });
 
   window.addEventListener('resize', resizeHandler);
