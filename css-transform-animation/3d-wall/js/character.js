67de5456036f9537b833f57a92883e9f4bf93623
DongHun, Kim <donghun.kdh@gmail.com>
2023-07-23 22:39:42 +0900

    저장소 통합

--- css-transform-animation/3d-wall/js/character.js
+++ css-transform-animation/3d-wall/js/character.js
@@ -32,8 +32,12 @@ function Character(info) {
   this.mainEl.style.left = `calc(${info.xPos}% - 5vw)`;
 
   this.scrollState = false;
+  this.runningState = false;
+  this.xPos = info.xPos;
+  this.speed = 0.3;
   this.lastScrollTop = 0;
-
+  this.direction;
+  this.rafID;
   this.init();
 }
 
@@ -61,5 +65,49 @@ Character.prototype = {
 
       self.lastScrollTop = window.scrollY;
     });
+
+    window.addEventListener('keydown', function (e) {
+      if (self.runningState) return;
+
+      if (e.key === 'ArrowLeft') {
+        self.direction = 'left';
+        self.mainEl.setAttribute('data-direction', 'left');
+        self.mainEl.classList.add('running');
+        self.run();
+        self.runningState = true;
+      } else if (e.key === 'ArrowRight') {
+        self.direction = 'right';
+        self.mainEl.setAttribute('data-direction', 'right');
+        self.mainEl.classList.add('running');
+        self.run();
+        self.runningState = true;
+      }
+    });
+
+    window.addEventListener('keyup', function (e) {
+      self.mainEl.classList.remove('running');
+      this.cancelAnimationFrame(self.rafID);
+      self.runningState = false;
+    });
+  },
+  run: function () {
+    const self = this;
+    if (self.direction === 'left') {
+      self.xPos -= self.speed;
+    } else if (self.direction === 'right') {
+      self.xPos += self.speed;
+    }
+
+    if (self.xPos < 10) {
+      self.xPos = 10;
+    }
+
+    if (self.xPos > 90) {
+      self.xPos = 90;
+    }
+
+    self.mainEl.style.left = `calc(${self.xPos}% - 5vw)`;
+
+    self.rafID = requestAnimationFrame(self.run.bind(self));
   },
 };
