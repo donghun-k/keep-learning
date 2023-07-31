d4b75634b9b9f608444267e97a9721e2f08d5183
DongHun, Kim <donghun.kdh@gmail.com>
2023-07-31 21:36:04 +0900

    저장소 통합

--- auth/passport/src/models/users.model.js
+++ auth/passport/src/models/users.model.js
@@ -16,6 +16,7 @@ const userSchema = mongoose.Schema({
   googleId: {
     type: String,
     unique: true,
+    sparse: true,
   },
 });
 
--- auth/passport/src/server.js
+++ auth/passport/src/server.js
@@ -10,6 +10,10 @@ const PORT = 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// 뷰 엔진 설정
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // MongoDB 연결
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -21,6 +25,14 @@ mongoose
   });
 
 app.use('/static', express.static(path.join(__dirname, 'public'))); // 정적 파일 위치 설정
+
+// 라우터 설정
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
