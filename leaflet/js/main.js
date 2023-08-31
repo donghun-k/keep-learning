5297be17b90cfa31b0b6469d554ba42deeee1648
DongHun, Kim <donghun.kdh@gmail.com>
2023-08-31 20:35:36 +0900

    저장소 통합

--- leaflet/js/main.js
+++ leaflet/js/main.js
@@ -1,15 +1,25 @@
 (() => {
   const pageEls = document.querySelectorAll('.page');
+  const closeBtnEl = document.querySelector('.close-btn');
   let pageCount = 0;
 
   pageEls.forEach((pageEl, i) => {
     if (i == 1) return;
-    pageEl.addEventListener('click', () => {
+    pageEl.addEventListener('click', (e) => {
+      if (e.target === closeBtnEl) return;
       pageEl.classList.add('page-flipped');
       pageCount++;
       if (pageCount === 2) {
         document.body.classList.add('leaflet-opened');
       }
     });
+  });
+
+  closeBtnEl.addEventListener('click', () => {
+    pageCount = 0;
+    pageEls[2].classList.remove('page-flipped');
+    setTimeout(() => {
+      pageEls[0].classList.remove('page-flipped');
+    }, 500);
   });
 })();
