18c5430f7eeaf2ce4f7a82528b720f1bd496b00b
DongHun, Kim <donghun.kdh@gmail.com>
2023-09-12 09:49:41 +0900

    저장소 통합

--- chat/websocket/client/index.js
+++ chat/websocket/client/index.js
@@ -1,1 +1,11 @@
 const ws = new WebSocket('ws://localhost:7071/ws');
+
+ws.onmessage = (webSocketMessage) => {
+  console.log(webSocketMessage);
+  console.log(webSocketMessage.data);
+};
+
+document.body.onmousemove = (event) => {
+  const { clientX, clientY } = event;
+  ws.send(JSON.stringify({ clientX, clientY }));
+};
--- chat/websocket/server/index.js
+++ chat/websocket/server/index.js
@@ -1,2 +1,10 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 7071 });
+
+wss.on('connection', (ws) => {
+  ws.send('Connected');
+  ws.on('message', (message) => {
+    message = JSON.parse(message);
+    console.log(message);
+  });
+});
