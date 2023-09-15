2606a8eece4a2d431c8a7fd12cec3dec7438acd5
DongHun, Kim <donghun.kdh@gmail.com>
2023-09-15 21:47:51 +0900

    저장소 통합

--- chat/websocket/src/index.js
+++ chat/websocket/src/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PUBLIC_DIR_PATH = path.join(__dirname, '../public');
 const PORT = 4000;
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('socket', socket);
+
+  socket.on('join', () => {});
+  socket.on('sendMessage', () => {});
+  socket.on('disconnect', () => {});
+});
 
 app.use(express.static(PUBLIC_DIR_PATH));
 
