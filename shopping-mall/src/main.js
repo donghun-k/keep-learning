89b8b4d5fba99c8bf52393811f8e2587847dc77f
DongHun, Kim <donghun.kdh@gmail.com>
2023-10-07 14:12:36 +0900

    저장소 통합

--- shopping-mall/src/main.js
+++ shopping-mall/src/main.js
@@ -1,7 +1,9 @@
-document.querySelector("#app").innerHTML = `
-  <div class="container mx-auto py-8">
-    <h1 class="text-5xl font-mono font-extrabold">
-      <span class="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Hello!</span>
-    </h1>
-  </div>
-`;
+async function main() {
+  const response = await fetch(
+    'https://learnwitheunjae.dev/api/sinabro-js/ecommerce'
+  );
+  const json = await response.json();
+  console.log(json);
+}
+
+main();
