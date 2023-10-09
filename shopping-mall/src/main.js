5b1627f558087f398861a73ef00aef81a4a223a0
DongHun, Kim <donghun.kdh@gmail.com>
2023-10-09 22:13:38 +0900

    저장소 통합

--- shopping-mall/src/main.js
+++ shopping-mall/src/main.js
@@ -1,34 +1,70 @@
 import test from './test.json?raw'; // vite의 기능, 파일의 내용을 string으로 가져옴
 
-async function getProducts() {
+const getProducts = async () => {
   if (process.env.NODE_ENV === 'development') {
     return JSON.parse(test);
   } else {
     const response = await fetch('/products.json');
     return response.json();
   }
-}
+};
 
-async function main() {
+const findEl = (startingEl, selector) => {
+  let currentEl = startingEl;
+  while (currentEl) {
+    if (currentEl.matches(selector)) {
+      return currentEl;
+    }
+    currentEl = currentEl.parentElement;
+  }
+  return null;
+};
+
+const main = async () => {
   const products = await getProducts();
   document.querySelector('#products').innerHTML = products
     .map(
-      (product) => `
-      <div class="product">
+      (product, i) => /* html */ `
+      <div class="product" data-product-id="${product.id}" data-product-index=${i}>
         <img src="${product.images[0]}" alt="${product.name}" />
         <p>${product.name}</p>
         <div class="flex items-center justify-between">
           <span>Price: ${product.regularPrice}</span>
           <div>
-            <button type="button" class="bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">-</button>
+            <button type="button" class="btn-decrease bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">-</button>
             <span class="text-green-800">0</span>
-            <button type="button" class="bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">+</button>
+            <button type="button" class="btn-increase bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">+</button>
           </div>
         </div>
       </div>
     `
     )
     .join('');
-}
+
+  document.querySelector('#products').addEventListener('click', (e) => {
+    const targetEl = e.target;
+    const productEl = findEl(targetEl, '.product');
+    const productId = productEl.dataset.productId;
+    const productIndex = productEl.dataset.productIndex;
+    const product = products[productIndex];
+
+    console.log(
+      'productId',
+      productId,
+      'productIndex',
+      productIndex,
+      'product',
+      product
+    );
+
+    console.log(productId);
+    if (targetEl.matches('.btn-decrease')) {
+      console.log('decrease!');
+    }
+    if (targetEl.matches('.btn-increase')) {
+      console.log('increase!');
+    }
+  });
+};
 
 main();
