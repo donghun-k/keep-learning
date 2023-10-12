1d5fad61d326fb7763e0388540d3f853ac249274
DongHun, Kim <donghun.kdh@gmail.com>
2023-10-12 23:33:01 +0900

    저장소 통합

--- shopping-mall/src/main.js
+++ shopping-mall/src/main.js
@@ -18,7 +18,7 @@ const getProductHTML = (product, cartCount = 0) => {
     <div class="flex items-center justify-between">
       <span>Price: ${product.regularPrice}</span>
       <div>
-        <button type="button" disabled class="btn-decrease disabled:cursor-not-allowed disabled:opacity-50 bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">-</button>
+        <button type="button" class="btn-decrease disabled:cursor-not-allowed disabled:opacity-50 bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">-</button>
         <span class="cart-count text-green-800">${cartCount}</span>
         <button type="button" class="btn-increase bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">+</button>
       </div>
@@ -45,6 +45,51 @@ const main = async () => {
     productMap[product.id] = product;
   });
   const countMap = {};
+
+  const updateProductCount = (productId) => {
+    const productEl = document.querySelector(
+      `.product[data-product-id="${productId}"]`
+    );
+    const cartCountEl = productEl.querySelector('.cart-count');
+    cartCountEl.textContent = countMap[productId];
+  };
+
+  const updateCart = () => {
+    const productsIds = Object.keys(countMap);
+
+    document.querySelector('.cart-items').innerHTML = productsIds
+      .map((productId) => {
+        const productInCart = productMap[productId];
+        return countMap[productId] === 0
+          ? ''
+          : getProductHTML(productInCart, countMap[productId]);
+      })
+      .join('');
+
+    document.querySelector('.total_count').innerHTML = Object.values(
+      countMap
+    ).reduce((acc, cur) => acc + cur, 0);
+  };
+
+  const increaseCount = (productId) => {
+    if (countMap[productId] === undefined) {
+      countMap[productId] = 0;
+    }
+    countMap[productId] += 1;
+    updateProductCount(productId);
+    updateCart();
+  };
+  const decreaseCount = (productId) => {
+    if (countMap[productId] === undefined) {
+      countMap[productId] = 0;
+    }
+    if (countMap[productId] > 0) {
+      countMap[productId] -= 1;
+      updateProductCount(productId);
+      updateCart();
+    }
+  };
+
   document.querySelector('#products').innerHTML = products
     .map((product) => getProductHTML(product))
     .join('');
@@ -53,48 +98,35 @@ const main = async () => {
     const targetEl = e.target;
     const productEl = findEl(targetEl, '.product');
     const productId = productEl.dataset.productId;
-    const product = productMap[productId];
 
     if (
       targetEl.matches('.btn-decrease') ||
       targetEl.matches('.btn-increase')
     ) {
-      if (countMap[productId] === undefined) {
-        countMap[productId] = 0;
-      }
       if (targetEl.matches('.btn-decrease')) {
-        if (countMap[productId] === 0) {
-          return;
-        }
-        countMap[productId] -= 1;
+        decreaseCount(productId);
       }
       if (targetEl.matches('.btn-increase')) {
-        countMap[productId] += 1;
+        increaseCount(productId);
       }
+    }
+  });
 
-      const btnDecreaseEl = productEl.querySelector('.btn-decrease');
+  document.querySelector('.cart-items').addEventListener('click', (e) => {
+    const targetEl = e.target;
+    const productEl = findEl(targetEl, '.product');
+    const productId = productEl.dataset.productId;
 
-      if (countMap[productId] === 0) {
-        btnDecreaseEl.setAttribute('disabled', true);
-      } else {
-        btnDecreaseEl.removeAttribute('disabled');
+    if (
+      targetEl.matches('.btn-decrease') ||
+      targetEl.matches('.btn-increase')
+    ) {
+      if (targetEl.matches('.btn-decrease')) {
+        decreaseCount(productId);
       }
-
-      const cartCountEl = productEl.querySelector('.cart-count');
-      cartCountEl.textContent = countMap[productId];
-
-      const productsIds = Object.keys(countMap);
-
-      document.querySelector('.cart-items').innerHTML = productsIds
-        .map((productId) => {
-          const productInCart = productMap[productId];
-          return getProductHTML(productInCart, countMap[productId]);
-        })
-        .join('');
-
-      document.querySelector('.total_count').innerHTML = Object.values(
-        countMap
-      ).reduce((acc, cur) => acc + cur, 0);
+      if (targetEl.matches('.btn-increase')) {
+        increaseCount(productId);
+      }
     }
   });
 
