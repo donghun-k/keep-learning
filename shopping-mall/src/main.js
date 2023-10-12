a9970d2059a8571fc18e7b976cbd59e725e01178
DongHun, Kim <donghun.kdh@gmail.com>
2023-10-12 22:34:24 +0900

    저장소 통합

--- shopping-mall/src/main.js
+++ shopping-mall/src/main.js
@@ -1,3 +1,4 @@
+import { get } from 'mobx';
 import test from './test.json?raw'; // vite의 기능, 파일의 내용을 string으로 가져옴
 
 const getProducts = async () => {
@@ -7,6 +8,23 @@ const getProducts = async () => {
     const response = await fetch('/products.json');
     return response.json();
   }
+};
+
+const getProductHTML = (product, cartCount = 0) => {
+  return /* html */ `
+  <div class="product" data-product-id="${product.id}">
+    <img src="${product.images[0]}" alt="${product.name}" />
+    <p>${product.name}</p>
+    <div class="flex items-center justify-between">
+      <span>Price: ${product.regularPrice}</span>
+      <div>
+        <button type="button" disabled class="btn-decrease disabled:cursor-not-allowed disabled:opacity-50 bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">-</button>
+        <span class="cart-count text-green-800">${cartCount}</span>
+        <button type="button" class="btn-increase bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">+</button>
+      </div>
+    </div>
+  </div>
+`;
 };
 
 const findEl = (startingEl, selector) => {
@@ -22,32 +40,20 @@ const findEl = (startingEl, selector) => {
 
 const main = async () => {
   const products = await getProducts();
+  const productMap = {};
+  products.forEach((product) => {
+    productMap[product.id] = product;
+  });
   const countMap = {};
   document.querySelector('#products').innerHTML = products
-    .map(
-      (product, i) => /* html */ `
-      <div class="product" data-product-id="${product.id}" data-product-index=${i}>
-        <img src="${product.images[0]}" alt="${product.name}" />
-        <p>${product.name}</p>
-        <div class="flex items-center justify-between">
-          <span>Price: ${product.regularPrice}</span>
-          <div>
-            <button type="button" disabled class="btn-decrease disabled:cursor-not-allowed disabled:opacity-50 bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">-</button>
-            <span class="cart-count text-green-800">0</span>
-            <button type="button" class="btn-increase bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">+</button>
-          </div>
-        </div>
-      </div>
-    `
-    )
+    .map((product) => getProductHTML(product))
     .join('');
 
   document.querySelector('#products').addEventListener('click', (e) => {
     const targetEl = e.target;
     const productEl = findEl(targetEl, '.product');
     const productId = productEl.dataset.productId;
-    const productIndex = productEl.dataset.productIndex;
-    const product = products[productIndex];
+    const product = productMap[productId];
 
     if (
       targetEl.matches('.btn-decrease') ||
@@ -77,12 +83,31 @@ const main = async () => {
       const cartCountEl = productEl.querySelector('.cart-count');
       cartCountEl.textContent = countMap[productId];
 
-      const cartCount = Object.values(countMap).reduce(
-        (acc, cur) => acc + cur,
-        0
-      );
-      document.querySelector('.total_count').innerHTML = `${cartCount}`;
+      const productsIds = Object.keys(countMap);
+
+      document.querySelector('.cart-items').innerHTML = productsIds
+        .map((productId) => {
+          const productInCart = productMap[productId];
+          return getProductHTML(productInCart, countMap[productId]);
+        })
+        .join('');
+
+      document.querySelector('.total_count').innerHTML = Object.values(
+        countMap
+      ).reduce((acc, cur) => acc + cur, 0);
     }
+  });
+
+  document.querySelector('.btn-cart').addEventListener('click', () => {
+    document.body.classList.add('displaying_cart');
+  });
+
+  document.querySelector('.btn-close-cart').addEventListener('click', () => {
+    document.body.classList.remove('displaying_cart');
+  });
+
+  document.querySelector('.cart-dimmed-bg').addEventListener('click', () => {
+    document.body.classList.remove('displaying_cart');
   });
 };
 
