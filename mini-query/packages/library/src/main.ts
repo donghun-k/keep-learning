fcb47ac996c290de4bd769dc876af436f936ceb0
DongHun, Kim <donghun.kdh@gmail.com>
2023-10-30 00:51:40 +0900

    저장소 통합

--- mini-query/packages/library/src/main.ts
+++ mini-query/packages/library/src/main.ts
@@ -1,21 +1,32 @@
-class MiniQuery {
-  private elements: Element[];
-  constructor(selector: string, container?: Element) {
-    this.elements = Array.from(
-      (container ?? document).querySelectorAll(selector)
-    );
-  }
+// class MiniQuery {
+//   private elements: Element[];
+//   constructor(selector: string, container?: Element) {
+//     this.elements = Array.from(
+//       (container ?? document).querySelectorAll(selector)
+//     );
+//   }
 
-  click(handler: EventListener) {
-    this.elements.forEach((element) => {
-      element.addEventListener('click', handler);
-    });
-  }
-  length() {
-    return this.elements.length;
-  }
-}
+//   click(handler: EventListener) {
+//     this.elements.forEach((element) => {
+//       element.addEventListener('click', handler);
+//     });
+//   }
+//   length() {
+//     return this.elements.length;
+//   }
+// }
 
 export const $ = (selector: string, container?: Element) => {
-  return new MiniQuery(selector, container);
+  const elements = Array.from(
+    (container ?? document).querySelectorAll(selector)
+  );
+
+  return {
+    length: () => elements.length,
+    click: (handler: EventListener) => {
+      elements.forEach((element) => {
+        element.addEventListener('click', handler);
+      });
+    },
+  };
 };
