ac3df6b2e392e5a87760e9a4a72d58b5dd0e9e81
DongHun, Kim <donghun.kdh@gmail.com>
2023-11-13 15:37:27 +0900

    저장소 통합

---
+++ blog/src/components/MarkdownViewer.tsx
@@ -0,0 +1,52 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Image from 'next/image';
+
+interface Props {
+  content: string;
+}
+
+const MarkdownViewer = ({ content }: Props) => {
+  return (
+    <ReactMarkdown
+      className="prose max-w-none"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code(props) {
+          const { children, className, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || '');
+          return match ? (
+            <SyntaxHighlighter
+              PreTag="div"
+              language={match[1]}
+              {...rest}
+              ref={null}
+              style={materialDark}
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            className="max-h-60 w-full object-cover"
+            src={image.src || ''}
+            alt={image.alt || ''}
+            width={500}
+            height={350}
+          />
+        ),
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  );
+};
+
+export default MarkdownViewer;
