8384b4f76406aa93f8bd6f9d5cd981c4b3d2910d
DongHun, Kim <donghun.kdh@gmail.com>
2023-11-13 13:31:12 +0900

    저장소 통합

--- blog/src/service/posts.ts
+++ blog/src/service/posts.ts
@@ -10,6 +10,10 @@ export interface Post {
   featured: boolean;
 }
 
+export interface PostData extends Post {
+  content: string;
+}
+
 export const getNonFeaturedPosts = async (): Promise<Post[]> => {
   return getAllPosts().then((posts) => posts.filter((post) => !post.featured));
 };
@@ -23,4 +27,16 @@ export const getAllPosts = async (): Promise<Post[]> => {
   return readFile(filePath, 'utf-8')
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+};
+
+export const getPostData = async (fileName: string): Promise<PostData> => {
+  const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
+  const metadata = await getAllPosts().then((posts) =>
+    posts.find((post) => post.path === fileName)
+  );
+  if (!metadata) {
+    throw new Error(`${fileName} not found`);
+  }
+  const content = await readFile(filePath, 'utf-8');
+  return { ...metadata, content };
 };
