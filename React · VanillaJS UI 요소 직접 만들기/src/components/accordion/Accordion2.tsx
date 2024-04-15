e52536046d7292303d682df7a19b7b7e78216544
DongHun, Kim <donghun.kdh@gmail.com>
2024-04-15 21:57:43 +0900

    UI 요소 직접 만들기) 아코디언 - CSS로 숨김, 보임 처리

---
+++ React · VanillaJS UI 요소 직접 만들기/src/components/accordion/Accordion2.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import cx from './cx';
+import data from './data';
+
+interface AccordionItemProps {
+  item: {
+    id: string;
+    title: string;
+    description: string;
+  };
+  current: boolean;
+  toggle: () => void;
+}
+
+const AccordionItem = ({ item, current, toggle }: AccordionItemProps) => {
+  return (
+    <li
+      onClick={toggle}
+      className={cx('item', 'item2', { current })}
+      key={item.id}
+    >
+      <div className={cx('tab')}>{item.title}</div>
+      <div className={cx('description')}>{item.description}</div>
+    </li>
+  );
+};
+
+const Accordion2 = () => {
+  const [currentId, setCurrentId] = useState<string | null>(data[0].id);
+  const toggleCurrent = (id: string) => () => {
+    setCurrentId((prev) => (prev === id ? null : id));
+  };
+  return (
+    <>
+      <h3>#2. React</h3>
+      <ul className={cx('container')}>
+        {data.map((item) => (
+          <AccordionItem
+            item={item}
+            current={currentId === item.id}
+            toggle={toggleCurrent(item.id)}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Accordion2;
--- React · VanillaJS UI 요소 직접 만들기/src/components/accordion/index.tsx
+++ React · VanillaJS UI 요소 직접 만들기/src/components/accordion/index.tsx
@@ -1,10 +1,12 @@
 import Accordion1 from './Accordion1';
+import Accordion2 from './Accordion2';
 import cx from './cx';
 
 const Accordions = () => {
   return (
     <div className={cx('Accordions')}>
       <Accordion1 />
+      <Accordion2 />
     </div>
   );
 };
