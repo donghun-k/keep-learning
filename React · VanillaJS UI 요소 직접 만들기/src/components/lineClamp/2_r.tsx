a7e2db38054beccc7874c0aa799cba75e2820f69
DongHun, Kim <donghun.kdh@gmail.com>
2024-05-07 22:52:10 +0900

    UI 요소 직접 만들기) 말줄임 - Clone Element

---
+++ React · VanillaJS UI 요소 직접 만들기/src/components/lineClamp/2_r.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from 'react';
+import cx from './cx';
+import data from './data';
+
+const LineClampText = ({ text }: { text: string }) => {
+  const textRef = useRef<HTMLDivElement>(null);
+  const cloneRef = useRef<HTMLDivElement>(null);
+  const [isClamped, toggleClamped] = useState(true);
+
+  useEffect(() => {
+    if (!text || !textRef.current || !cloneRef.current) return;
+    toggleClamped(
+      cloneRef.current.offsetHeight /
+        parseInt(getComputedStyle(textRef.current).lineHeight) >
+        3
+    );
+  }, [text]);
+
+  return (
+    <div className={cx('content', { clamped: isClamped })}>
+      <div className={cx('text-clone')} ref={cloneRef}>
+        {text}
+      </div>
+      <div className={cx('text')} ref={textRef} style={{ WebkitLineClamp: 3 }}>
+        {text}
+      </div>
+      {isClamped && (
+        <button
+          className={cx('buttonMore')}
+          onClick={() => {
+            toggleClamped(false);
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+const LineClamp2 = () => {
+  return (
+    <>
+      <h3>
+        #2<sub>Clone - 3줄 말줄임</sub>
+      </h3>
+      {data.map((text, index) => (
+        <LineClampText key={index} text={text} />
+      ))}
+    </>
+  );
+};
+
+export default LineClamp2;
