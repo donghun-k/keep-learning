b7f7d96e2a9328222a5091380edc52e9d648bd7d
DongHun, Kim <donghun.kdh@gmail.com>
2024-05-21 23:08:24 +0900

    -진짜 왕초보 iOS 배우기-

---
+++ 진짜 왕초보 iOS 배우기/PetInfoApp/PetInfoApp/PetInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetInfoAppApp.swift
+//  PetInfoApp
+//
+//  Created by DorMon on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PetInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
