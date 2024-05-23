2cc1285303d7ef006679b1bd218394a719b46bc7
DongHun, Kim <donghun.kdh@gmail.com>
2024-05-23 22:57:38 +0900

    PetInfoApp) VStack, HStack으로 뷰 구성하기

--- 진짜 왕초보 iOS 배우기/PetInfoApp/PetInfoApp/ContentView.swift
+++ 진짜 왕초보 iOS 배우기/PetInfoApp/PetInfoApp/ContentView.swift
@@ -1,36 +1,62 @@
-//
-//  ContentView.swift
-//  PetInfoApp
-//
-//  Created by DorMon on 5/21/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        HStack {
-            Image("dogProfile")
-                .resizable()
-                .frame(width: 120, height: 120)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                .overlay(
-                    Circle()
-                        .stroke(Color.teal,
-                                lineWidth: 3)
-                )
-            VStack(alignment: .leading) {
-                Text("이름: 멍뭉이")
-                Text("시바견")
-                Text("2024년 12월생")
-            }
-            .font(.system(size: 20))
-            .padding(.leading, 20)
+  var body: some View {
+    VStack {
+      HStack {
+        Image("dogProfile")
+          .resizable()
+          .frame(width: 120, height: 120)
+          .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+          .overlay(
+            Circle()
+              .stroke(Color.teal,
+                      lineWidth: 3)
+          )
+        VStack(alignment: .leading) {
+          Text("이름: 멍뭉이")
+          Text("시바견")
+          Text("2024년 12월생")
         }
-
+        .font(.system(size: 20))
+        .padding(.leading, 20)
+      }
+      .padding(.bottom, 20)
+      VStack(alignment: .leading, spacing: 10) {
+        Text("좋아하는 것")
+          .font(.system(size: 20))
+          .bold()
+        Divider()
+        Text(" - 햄버거")
+        Text(" - 토끼")
+      }
+      .padding()
+      .background(
+        RoundedRectangle(cornerRadius: 15)
+          .fill(Color.mint)
+          .opacity(0.2)
+          .shadow(radius: 5)
+      )
+      VStack(alignment: .leading, spacing: 10) {
+        Text("스킬")
+          .font(.system(size: 20))
+          .bold()
+        Divider()
+        Text(" - 울기")
+        Text(" - 안아주기")
+      }
+      .padding()
+      .background(
+        RoundedRectangle(cornerRadius: 15)
+          .fill(Color.pink)
+          .opacity(0.2)
+          .shadow(radius: 5)
+      )
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
