fe547c7b372e5c778d83903ee362b8d1f5a66bf8
DongHun, Kim <donghun.kdh@gmail.com>
2024-06-01 23:15:39 +0900

    UI 요소 직접 만들기) Scroll Spy - Scroll Event로 감지

--- React · VanillaJS UI 요소 직접 만들기/src/components/scrollSpy/1_r.tsx
+++ React · VanillaJS UI 요소 직접 만들기/src/components/scrollSpy/1_r.tsx
@@ -1,5 +1,11 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
 import cx from './cx';
 import data from './data';
+import ViewportContextProvider, {
+  useViewportRect,
+} from '@/context/viewportContext';
+
+const HEADER_HEIGHT = 60;
 
 interface ListItemProps {
   id: string;
@@ -8,20 +14,96 @@ interface ListItemProps {
   description: string;
 }
 
-const ListItem = ({ number, title, description }: ListItemProps) => {
+type ItemInfo = {
+  index: number;
+  top: number;
+  height: number;
+  el: HTMLElement;
+} | null;
+
+const ListItem = ({ number, title, description, id }: ListItemProps) => {
   return (
-    <li>
+    <li id={id} data-number={number}>
       <p>
         <strong>
           {number}. {title}
         </strong>
       </p>
-      <div>{description}</div>
+      <div>
+        {description.split('\r\n').map((line, i) => (
+          <p key={i}>{line}</p>
+        ))}
+      </div>
     </li>
   );
 };
 
 const ScrollSpy1 = () => {
+  return (
+    <ViewportContextProvider>
+      <ScrollSpy />
+    </ViewportContextProvider>
+  );
+};
+
+const ScrollSpy = () => {
+  const { top: viewportTop, scrollHeight } = useViewportRect();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const itemsRef = useRef<ItemInfo[]>([]);
+  const navsRef = useRef<(HTMLLIElement | null)[]>([]);
+
+  const setCurrentItem = useCallback(() => {
+    const scrollTop = viewportTop * -1;
+    const target = itemsRef.current.find(
+      (item) =>
+        item &&
+        scrollTop >= item.top - HEADER_HEIGHT - item.height / 2 &&
+        scrollTop < item.top - HEADER_HEIGHT + item.height / 2
+    );
+    if (target) {
+      setCurrentIndex(target.index);
+      navsRef.current[target.index]?.scrollIntoView({
+        block: 'nearest',
+        inline: 'center',
+        behavior: 'instant',
+      });
+    }
+  }, [viewportTop]);
+
+  const handleNavClick = useCallback((index: number) => {
+    const itemY = (itemsRef.current[index]?.top || 0) - HEADER_HEIGHT;
+    if (itemY >= 0) {
+      window.scrollTo({
+        top: itemY,
+        behavior: 'smooth',
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const calculateItem = () => {
+      const scrollTop = document.scrollingElement!.scrollTop;
+      itemsRef.current = data.map((d, i) => {
+        const $item = document.getElementById(d.id);
+        if (!$item) return null;
+        const { top, height } = $item.getBoundingClientRect();
+        return { el: $item, index: i, top: top + scrollTop, height };
+      });
+    };
+    calculateItem();
+
+    const resizeObserver = new ResizeObserver(calculateItem);
+    resizeObserver.observe(document.scrollingElement!);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    setCurrentItem();
+  }, [setCurrentItem, viewportTop]);
+
   return (
     <div className={cx('ScrollSpy')}>
       <header className={cx('floatingHeader')}>
@@ -29,15 +111,21 @@ const ScrollSpy1 = () => {
         <h3 className={cx('subTitle')}>#1. React</h3>
         <ul className={cx('nav')}>
           {data.map((item, i) => (
-            <li key={i} className={cx('navItem')}>
-              <button>{i + 1}</button>
+            <li
+              key={i}
+              className={cx('navItem', { current: currentIndex === i })}
+              ref={(r) => (navsRef.current[i] = r)}
+            >
+              <button onClick={() => handleNavClick(i)}>{i + 1}</button>
             </li>
           ))}
         </ul>
       </header>
-      {data.map((item, i) => (
-        <ListItem key={i} {...item} number={i + 1} />
-      ))}
+      <ul>
+        {data.map((item, i) => (
+          <ListItem key={i} {...item} number={i + 1} />
+        ))}
+      </ul>
     </div>
   );
 };
